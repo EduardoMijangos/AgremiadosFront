d2a8b6272907dbebb7de0ed98893f396645d356e
Eduardo <lalomijafru126@gmail.com>
2023-11-14 04:09:57 -0600

    concexion con el backend

--- src/app/components/veragremiado/veragremiado.component.ts
+++ src/app/components/veragremiado/veragremiado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-veragremiado',
@@ -6,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./veragremiado.component.scss'],
 })
 export class VeragremiadoComponent  implements OnInit {
+  agremiados: any[] = [];  
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.obtenerAgremiados();  // Llama a la función para obtener agremiados al iniciar el componente
+  }
 
+  obtenerAgremiados() {
+    this.authService.getAgremiados().subscribe(
+      (data) => {
+        this.agremiados = data;
+        console.log('Agremiados obtenidos con éxito', this.agremiados);
+      },
+      (error) => {
+        console.error('Error al obtener agremiados', error);
+      }
+    );
+  }
 }
